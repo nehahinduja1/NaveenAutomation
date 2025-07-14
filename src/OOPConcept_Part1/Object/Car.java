922ad07241183ad9f23b57353b239855b2a76b0b
ATTAQA <nehahinduja29@gmail.com>
2025-07-14 18:05:11 +0100

    Object

---
+++ src/OOPConcept_Part1/Object/Car.java
@@ -0,0 +1,45 @@
+package OOPConcept_Part1.Object;
+
+public class Car {
+
+//    How to create object of Car class
+
+// create two variable called as Class variables : global variables
+
+    int mod;
+    int wheel;
+
+// create an object of Car class -> created 3 object of car class
+
+    public static void main(String[] args) {
+
+        // new Car()-> object of car class-> this line is representing one object
+        // Car is a class name so C capital
+        // object reference variable a,b,c; they are representing object
+        // new keyword is used to create the object
+
+        Car a = new Car();
+        Car b = new Car();
+        Car c = new Car();
+
+        a.mod = 2015;  // initializing values
+        a.wheel = 4;  // object ref var.class var
+
+        b.mod = 2014;
+        b.wheel = 4;
+
+        c.mod = 2013;
+        c.wheel = 4;
+
+        System.out.println(a.mod);
+        System.out.println(a.wheel);
+
+        System.out.println(b.mod);
+        System.out.println(b.wheel);
+
+        System.out.println(c.mod);
+        System.out.println(c.wheel);
+
+
+    }
+}
